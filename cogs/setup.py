084c0e767dfbcb39d759f73f9fc37319110f7ad2
SubwayMan <tyche4490@gmail.com>
2022-07-14 00:10:46 -0700

    skeleton work

--- cogs/setup.py
+++ cogs/setup.py
@@ -6,7 +6,15 @@ class Setup(discord.Cog):
         self.bot = bot
         self._last_member = None
 
-    @discord.Cog.listener(name="on_message")
-    async def test(ctx, message):
-        await message.pin()
-        await message.channel.send("nice post dawg")
+    @discord.slash_command(name="initialize", description="Setup the Escape Room framework. Command only available for administrators.")
+    async def initialize(self, ctx):
+        await ctx.respond("Running setup.")
+
+    @discord.slash_command(name="checkhealth", description="Verify the functionality of all the escape room components. Command only available for administrators.")
+    async def check_health(self, ctx):
+        await ctx.respond("Checking health.")
+
+    @discord.slash_command(name="createroom", description="Add a room to the escape room. Command only available for administrators.")
+    async def create_room(self, ctx):
+        await ctx.respond("Creating room.")
+    
