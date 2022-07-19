d1a6b978eaf23ea528670004c023551000131ae0
SubwayMan <tyche4490@gmail.com>
2022-07-19 16:15:57 -0700

    worked with perms

--- cogs/setup.py
+++ cogs/setup.py
@@ -1,4 +1,6 @@
 import discord
+from discord.ext.commands import has_permissions 
+from .tools import false_perms, true_perms
 import pymongo
 
 
@@ -7,14 +9,26 @@ class Setup(discord.Cog):
         self.bot = bot
         self._last_member = None
         self.mongo_client = mongo_client
+    
+    @discord.slash_command(
+        name="initialize",
+        description="Setup the Escape Room framework. Command only available for administrators.",
+        default_member_permissions=discord.Permissions(administrator=True)
+    )
 
-    @discord.slash_command(name="initialize", description="Setup the Escape Room framework. Command only available for administrators.")
     async def initialize(self, ctx: discord.ApplicationContext):
         guild = ctx.guild
         victory_role = await guild.create_role(name="Escapist")
         role = await guild.create_role(name="Escape Room Participant")
-        perms = discord.PermissionOverwrite()
-        category = await guild.create_category("Escape Room", overwrites={role: perms})
+        bot_role = discord.utils.find(lambda r: r.is_bot_managed() and 
+            r in ctx.guild.get_member(self.bot.user.id).roles,
+            ctx.guild.roles)
+
+        category = await guild.create_category("Escape Room", overwrites={
+            bot_role: discord.PermissionOverwrite(view_channel=True, send_messages=True),
+            ctx.guild.default_role: discord.PermissionOverwrite(view_channel=False),
+            role: discord.PermissionOverwrite(view_channel=True, send_messages=False),
+        })
         channel = await category.create_text_channel("Room-1")
 
 
@@ -35,11 +49,21 @@ class Setup(discord.Cog):
         })
         await ctx.respond("Running setup.")
 
-    @discord.slash_command(name="checkhealth", description="Check the integrity of the escape room. Command only available for administrators.")
+    @discord.slash_command(
+        name="checkhealth", 
+        description="Check the integrity of the escape room. Command only available for administrators.",
+        default_member_permissions=discord.Permissions(administrator=True)
+    )
+
     async def check_health(self, ctx: discord.ApplicationContext):
         await ctx.respond("Checking health.")
 
-    @discord.slash_command(name="createroom", description="Add a room to the escape room. Command only available for administrators.")
+    @discord.slash_command(
+        name="createroom", 
+        description="Add a room to the escape room. Command only available for administrators.",
+        default_member_permissions=discord.Permissions(administrator=True)
+    )
+
     async def create_room(self, ctx: discord.ApplicationContext):
         if not self.check_validity(ctx.guild):
             await ctx.respond("This server does not contain a valid escape room.")
@@ -69,19 +93,26 @@ class Setup(discord.Cog):
 
         await ctx.respond("Creating room.")
 
-    @discord.slash_command(name="reset", description="Clears an escape room and all its components. Command only available for administrators.")
+    @discord.slash_command(
+        name="reset", 
+        description="Clears an escape room and all its components. Command only available for administrators.",
+        default_member_permissions=discord.Permissions(administrator=True)
+    )
+
     async def room_purge(self, ctx: discord.ApplicationContext):
         value = self.mongo_client.EscapeRoom.production.find_one({"guild_id": ctx.guild_id})
         if value:
-            role = discord.utils.get(ctx.guild.roles, id=value["role_id"])
-            await role.delete()
             for room in value["rooms"]:
-                role = discord.utils.get(ctx.guild.roles, id=room["role_id"])
-                await role.delete()
                 channel = discord.utils.get(ctx.guild.channels, id=room["channel_id"])
-                await channel.delete()
+                await self.delete_channel(channel)
+                role = discord.utils.get(ctx.guild.roles, id=room["role_id"])
+                await self.delete_role(role)
+
+            role = discord.utils.get(ctx.guild.roles, id=value["role_id"])
+            await self.delete_role(role)
+
             category = discord.utils.get(ctx.guild.categories, id=value["category_id"])
-            await category.delete()
+            await self.delete_category(category)
 
         self.mongo_client.EscapeRoom.production.delete_one({"guild_id": ctx.guild_id})
 
@@ -100,5 +131,17 @@ class Setup(discord.Cog):
         for room in guild_db["rooms"]:
             channels.append(room["channel_id"])
         return channel.id in channels
+
+    async def delete_role(self, role: discord.Role):
+        if role:
+            await role.delete()
+
+    async def delete_channel(self, channel: discord.TextChannel):
+        if channel:
+            await channel.delete()
+
+    async def delete_category(self, category: discord.CategoryChannel):
+        if category:
+            await category.delete()
 
 
--- cogs/tools.py
+++ cogs/tools.py
@@ -1,20 +1,36 @@
 import discord
 
 
-def admin_command():
-    """Decorator used to mark a slash command as
-    admin-only."""
-    pass
-
-
-def inspect_guild(guild: discord.Guild):
-    """Verifies the integrity of a guild's escape room components,
-    or whether or not they exist."""
-    status = {
-        "RoomCategoryExists": False,
-        "RoomIntegrity": 0,
-        "RoleIntegrity": 0
+def false_perms():
+    """Returns a dict with all discord channel
+    permissions set to False."""
+    return {
+        "view_channel": False,
+        "manage_channels": False,
+        "manage_permissions": False,
+        "manage_webhooks": False,
+        "create_instant_invite": False,
+        "send_messages": False,
+        "send_messages_in_threads": False,
+        "create_public_threads": False,
+        "create_private_threads": False,
+        "embed_links": False,
+        "attach_files": False,
+        "add_reactions": False,
+        "external_emojis": False,
+        "external_stickers": False,
+        "mention_everyone": False,
+        "manage_messages": False,
+        "manage_threads": False,
+        "read_message_history": False,
+        "send_tts_messages": False,
+        "use_application_commands": False
     }
+
+def true_perms():
+    """Opposite of false_perms."""
+    return dict((k, not v) for k, v in false_perms())
+
 
 
 
