ce3442f989b333661e75e8ee0d74e54c65939dd5
SubwayMan <tyche4490@gmail.com>
2022-09-04 16:55:02 -0700

    added temporary join game command, purged old answer command

--- main.py
+++ main.py
@@ -30,5 +30,18 @@ async def modaltest(ctx):
     await ctx.channel.send(view=AnswerModalView(config.DATABASE))
     await ctx.respond("View successfully created", ephemeral=True)
 
+@bot.command()
+async def joingame(ctx):
+    guild_db = config.DATABASE.find_one({"guild_id": ctx.guild_id})
+    if not guild_db:
+        ctx.send_response("No escape room set up in this server.", ephemeral=True)
+        return
+    role = ctx.guild.get_role(guild_db["rooms"][0]["role_id"])
+    await ctx.user.add_roles(role)
+    
+
+    
+
+
 bot.run(config.TOKEN)
 
